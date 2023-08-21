3958a8b033ccb66171d0d301785a5aeb0a99487f
Blaise <blaze.kalonda@gmail.com>
2023-08-22 01:58:37 +0200

    envGen update et worpress script

--- srcs/requirements/tools/envGen.sh
+++ srcs/requirements/tools/envGen.sh
@@ -19,8 +19,6 @@ function ctrl_c() {
   exit 1
 }
 
-
-
 # Check if .env file exists in project
 if [ -f "$ENV_PATH" ]; then
   echo -e "${YELLOW}.env file already exists${NC}"
@@ -38,22 +36,34 @@ if [ -f "$ENV_PATH" ]; then
 fi
 
 # Fill .env variables with prompt input
-echo -e "\n${GREY}Enter your database root password:${NC}"
-read -r MYSQL_ROOT_PASSWORD
-echo -e "\n${GREY}Enter your database password:${NC}"
+
+echo -e "\n${GREY}Enter your database user name:${NC}"
+read -r MYSQL_USER
+echo -e "\n${GREY}Enter your database user password:${NC}"
 read -r MYSQL_PASSWORD
 echo -e "\n${GREY}Enter your database name:${NC}"
 read -r MYSQL_DATABASE
-echo -e "\n${GREY}Enter your database user:${NC}"
-read -r MYSQL_USER
+echo -e "\n${GREY}Enter your database root password:${NC}"
+read -r MYSQL_ROOT_PASSWORD
+echo -e "\n${GREY}Enter your wp user name:${NC}"
+read -r WP_USER
+echo -e "\n${GREY}Enter your wp user password:${NC}"
+read -r WP_PASSWORD
+echo -e "\n${GREY}Enter your wp user email:${NC}"
+read -r WP_EMAIL
 echo -e "\n${GREY}Enter your wp admin user:${NC}"
 read -r WP_ADMIN_USER
 echo -e "\n$(GREY)Enter your wp admin password:${NC}"
 read -r WP_ADMIN_PASSWORD
+echo -e "\n${GREY}Enter your wp admin email:${NC}"
+read -r WP_ADMIN_EMAIL
 
 
 # if .env not filled exit
-if [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] || [ -z "$WP_ADMIN_USER" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$DOMAIN" ] ; then
+if [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] ||
+ [ -z "$WP_ADMIN_USER" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$DOMAIN" ] || [ -z "$WP_USER" ] ||
+  # shellcheck disable=SC1073
+  [ -z "$WP_PASSWORD"] || [ -z "$WP_EMAIL"] || [-z "$WP_ADMIN_EMAIL" ] ; then
   echo -e "\n${RED}Error: .env file not filled${NC}"
   exit 1
 else
@@ -65,7 +75,10 @@ else
   echo -e "WP_ADMIN_USER=$WP_ADMIN_USER" >> $ENV_PATH
   echo -e "WP_ADMIN_PASSWORD=$WP_ADMIN_PASSWORD" >> $ENV_PATH
   echo -e "DOMAIN=$DOMAIN" >> "$ENV_PATH"
-
+  echo -e "WP_USER=$WP_USER" >> "$ENV_PATH"
+  echo -e "WP_PASSWORD=$WP_PASSWORD" >> "$ENV_PATH"
+  echo -e "WP_EMAIL=$WP_EMAIL" >> "$ENV_PATH"
+  echo -e "WP_ADMIN_EMAIL=$WP_ADMIN_EMAIL" >> "$ENV_PATH"
 
   # Success message
   echo -e "\n${GREEN}Success!${NC} .env file created"
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 
 mkdir -p /var/www/html
 cd /var/www/html
@@ -11,9 +11,9 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 
 wp core config \
-	--dbname=$MARIADB_DB_NAME \
-	--dbuser=$MARIADB_USER \
-	--dbpass=$MARIADB_PASS \
+	--dbname=$MYSQL_DATABASE \
+	--dbuser=$MYSQL_USER \
+	--dbpass=$MYSQL_PASSWORD \
 	--dbhost=mariadb \
 	--allow-root
 
@@ -21,13 +21,16 @@ wp config set "WP_REDIS_HOST" "redis" --allow-root
 wp config set "WP_REDIS_PORT" "6379" --allow-root
 
 wp core install \
+  --url=$DOMAIN \
+  --title="ft_server" \
 	--admin_name=$WP_ADMIN_USER \
 	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL \
 	--allow-root
 
-wp user create toto \
+wp user create $WP_USER $WP_EMAIL \
 	--role=author \
-	--user_pass=1234 \
+	--user_pass=$WP_PASSWORD \
 	--allow-root
 
 wp plugin install redis-cache --activate --allow-root
