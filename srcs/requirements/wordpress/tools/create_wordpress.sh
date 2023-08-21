c067e5792f9cfe22474b12345c3ee1603e2326ab
Blaise <blaze.kalonda@gmail.com>
2023-08-22 00:08:35 +0200

    try new config

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,28 +1,37 @@
-#!/bin/sh
+# !/bin/bash
 
-# check if wp-config.php existe
-if [ -f ./wp-config.php ]; then
-    echo "WordPress is already downloaded."
-	exit 0
-else
+mkdir -p /var/www/html
+cd /var/www/html
 
-####### MANDATORY PART ##########
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-    # Download wordpress
-    wget http://wordpress.org/latest.tar.gz
-    tar xfz latest.tar.gz
-    mv wordpress/* .
-    rm -rf latest.tar.gz
-    rm -rf wordpress
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-    # Import env variables 
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-    cp wp-config-sample.php wp-config.php
-###################################
+wp core download --allow-root
 
-fi
+wp core config \
+	--dbname=$MARIADB_DB_NAME \
+	--dbuser=$MARIADB_USER \
+	--dbpass=$MARIADB_PASS \
+	--dbhost=mariadb \
+	--allow-root
 
-exec "$@"
+wp config set "WP_REDIS_HOST" "redis" --allow-root
+wp config set "WP_REDIS_PORT" "6379" --allow-root
+
+wp core install \
+	--admin_name=$WP_ADMIN_USER \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--allow-root
+
+wp user create toto \
+	--role=author \
+	--user_pass=1234 \
+	--allow-root
+
+wp plugin install redis-cache --activate --allow-root
+
+mkdir -p /run/php/
+wp redis enable --allow-root
+/usr/sbin/php-fpm7.4 -F
