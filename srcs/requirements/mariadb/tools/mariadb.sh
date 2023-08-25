753b122e24ecc89f2a63276b48fb803ec8e6eaca
Blaise <blaze.kalonda@gmail.com>
2023-08-25 17:54:38 +0200

    wordpress sh {}

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /srcs/.env
+source ./srcs/.env
 
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
@@ -8,24 +8,24 @@ mkdir -p /run/mysql
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-mysqld --user=${MYSQL_USER} --datadir=/var/lib/mysql --dbhost=${MYSQL_HOST} &
+mysqld --user=$MYSQL_USER --datadir=/var/lib/mysql --dbhost=$MYSQL_HOST &
 
 pid=$!
 
 # Wait for MariaDB to become available
-until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
+until mysqladmin -u root -p$MYSQL_ROOT_PASSWORD ping >/dev/null 2>&1; do
     sleep 1
 done
 
 # create wordpress database and user
 # modify user privileges on the database
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysql -u root -p$MYSQL_ROOT_PASSWOR -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
 # kill and restart database
 kill "$pid"
 wait "$pid"
-exec mysqld --user=${MYSQL_USER}
+exec mysqld --user=$MYSQL_USER
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /srcs/.env
+source ./srcs/.env
 mkdir -p /var/www/html
 cd /var/www/html
 
@@ -12,41 +12,41 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 
 # Affichage des valeurs des variables
-echo "MYSQL_DATABASE=${MYSQL_DATABASE}"
-echo "MYSQL_USER=${MYSQL_USER}"
-echo "MYSQL_PASSWORD=${MYSQL_PASSWORD}"
-echo "MYSQL_HOST=${MYSQL_HOST}"
-echo "DOMAIN=${DOMAIN}"
-echo "WP_ADMIN_USER=${WP_ADMIN_USER}"
-echo "WP_ADMIN_PASSWORD=${WP_ADMIN_PASSWORD}"
-echo "WP_ADMIN_EMAIL=${WP_ADMIN_EMAIL}"
-echo "WP_USER=${WP_USER}"
-echo "WP_PASSWORD=${WP_PASSWORD}"
-echo "WP_EMAIL=${WP_EMAIL}"
+echo "MYSQL_DATABASE=$MYSQL_DATABASE"
+echo "MYSQL_USER=$MYSQL_USER"
+echo "MYSQL_PASSWORD=$MYSQL_PASSWORD"
+echo "MYSQL_HOST=$MYSQL_HOST"
+echo "DOMAIN=$DOMAIN"
+echo "WP_ADMIN_USER=$WP_ADMIN_USER"
+echo "WP_ADMIN_PASSWORD=$WP_ADMIN_PASSWORD"
+echo "WP_ADMIN_EMAIL=$WP_ADMIN_EMAIL"
+echo "WP_USER=$WP_USER"
+echo "WP_PASSWORD=$WP_PASSWORD"
+echo "WP_EMAIL=$WP_EMAIL"
 
 # Crée le fichier de configuration wp-config.php
 wp core config \
-	--dbname=${MYSQL_DATABASE} \
-	--dbuser=${MYSQL_USER} \
-	--dbpass=${MYSQL_PASSWORD} \
-	--dbhost=${MYSQL_HOST} \
+	--dbname=$MYSQL_DATABASE \
+	--dbuser=$MYSQL_USER \
+	--dbpass=$MYSQL_PASSWORD \
+	--dbhost=$MYSQL_HOST \
 	--allow-root
 
 wp config set "WP_REDIS_HOST" "redis" --allow-root
 wp config set "WP_REDIS_PORT" "6379" --allow-root
 
 wp core install \
-  --url=${DOMAIN} \
+  --url=$DOMAIN \
   --title='ft_wordpress' \
-	--admin_name=${WP_ADMIN_USER} \
-	--admin_password=${WP_ADMIN_PASSWORD} \
-	--admin_email=${WP_ADMIN_EMAIL} \
+	--admin_name=$WP_ADMIN_USER \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL \
 	--allow-root
 
-wp user create ${WP_USER} \
+wp user create $WP_USER \
 	--role=author \
-	--user_pass=${WP_PASSWORD} \
-	--user_email=${WP_EMAIL} \
+	--user_pass=$WP_PASSWORD \
+	--user_email=$WP_EMAIL \
 	--allow-root
 
 wp plugin install redis-cache --activate --allow-root
