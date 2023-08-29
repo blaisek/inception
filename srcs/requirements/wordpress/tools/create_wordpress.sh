ad3244e26a731e1df7730767b44da5e0e2554d35
Blaise <blaze.kalonda@gmail.com>
2023-08-29 13:53:56 +0200

    wp title

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -20,7 +20,7 @@ wp core config \
 
 wp core install \
   --url="https://$DOMAIN" \
-  --title=$WP_TITLE \
+  --title="$WP_TITLE" \
 	--admin_name=$WP_ADMIN_USER \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=$WP_ADMIN_EMAIL \
