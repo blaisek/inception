76bec6e29b2b5b32369d5eddf3ac62bbd37a2c20
Blaise <blaze.kalonda@gmail.com>
2023-09-02 23:29:10 +0200

    Revert "wp title"
    This reverts commit 6bead03026432f99a99f2ba353344e657592bc1c.

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -20,7 +20,7 @@ wp core config \
 
 wp core install \
   --url="https://$DOMAIN" \
-  --title=$WP_TITLE \
+  --title="$WP_TITLE" \
 	--admin_name=$WP_ADMIN_USER \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=$WP_ADMIN_EMAIL \
@@ -31,9 +31,6 @@ wp user create $WP_USER \
 	--role=author \
 	--user_pass=$WP_PASSWORD \
 	--allow-root
-
-
-echo " title : ${WP_TITLE}"
 
 #wp plugin install redis-cache --activate --allow-root
 
