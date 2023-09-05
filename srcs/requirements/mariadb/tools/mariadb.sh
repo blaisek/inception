f184d6347f8405568af47d1c9dc7f5868bd8e684
Blaise Tchimanga <btchiman@c2r9s5.42lausanne.ch>
2023-09-05 19:44:36 +0200

    maria sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-mysql_upgrade
+mysql_install_db --datadir=/var/lib/mysql
 
-mysql_install_db  --datadir=/var/lib/mysql
-
-
-mkdir -p /run/mysql
-chown -R mysql:mysql /run/mysqld
+# Changer les autorisations du répertoire de données
 chown -R mysql:mysql /var/lib/mysql
 
-mysqld  --datadir=/var/lib/mysql &
+# Créer le répertoire /run/mysqld s'il n'existe pas
+mkdir -p /run/mysqld
+
+# Changer les autorisations du répertoire /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+mysqld --datadir=/var/lib/mysql &
 
 pid=$!
 
-# Wait for MariaDB to become available
+# Attendre que MariaDB soit disponible
 until mysqladmin -u root -p$MARIADB_ROOT_PASSWORD ping >/dev/null 2>&1; do
     sleep 1
 done
 
-# create wordpress database and user
-# modify user privileges on the database
+# Créer la base de données WordPress et l'utilisateur
+# Modifier les privilèges de l'utilisateur sur la base de données
 mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;"
 mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MARIADB_USER' IDENTIFIED BY '$MARIADB_PASSWORD';"
 mysql -u root -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_USER';"
 mysql -u root -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 mysql -u root -p$MARIADB_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
 
-# kill and restart database
+# Arrêter et redémarrer la base de données
 kill "$pid"
 wait "$pid"
 exec mysqld --datadir=/var/lib/mysql
