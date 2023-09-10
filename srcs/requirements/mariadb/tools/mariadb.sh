a7f447738c55166309605e15a67f6c2ae034993d
Blaise <blaze.kalonda@gmail.com>
2023-09-10 03:11:28 +0200

    maria script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,34 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-#mysql_install_db --datadir=/var/lib/mysql
+# Checking if the database is already created
+cat .setup 2>/dev/null
 
-# Changer les autorisations du répertoire de données
-#chown -R mysql:mysql /var/lib/mysql
+if [ $? -ne 0 ]; then
 
-# Créer le répertoire /run/mysqld s'il n'existe pas
-#mkdir -p /run/mysqld
+  # Run the server in the background and wait for it to start up before creating the database
+  mariadb-install-db --datadir=/var/lib/mysql \
+    --auth-root-authentication-method=normal
 
-# Changer les autorisations du répertoire /run/mysqld
-#chown -R mysql:mysql /run/mysqld
+  # Give permissions to the database for the user mysql
+  chown -R mysql:mysql /var/lib/mysql
+  chown -R mysql:mysql /run/mysqld
 
-#mysqld --datadir=/var/lib/mysql &
+  # Start the server
+  mysqld_safe --datadir=/var/lib/mysql &
 
-#pid=$!
-
-# Attendre que MariaDB soit disponible
-until mysqladmin -u root -p$MARIADB_ROOT_PASSWORD ping >/dev/null 2>&1; do
+  # Wait until mariadb is ready to accept connections
+  while ! mysqladmin ping -h "mariadb" --silent; do
     sleep 1
-done
+  done
 
-# Créer la base de données WordPress et l'utilisateur
-# Modifier les privilèges de l'utilisateur sur la base de données
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS "test";"
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MARIADB_USER' IDENTIFIED BY '$MARIADB_PASSWORD';"
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_USER';"
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-mysql -u root -p$MARIADB_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
+  # Create database from .sql file
+  eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+  touch .setup
+else
+  echo "Database already created and ready"
+fi
 
-# Arrêter et redémarrer la base de données
-mysqladmin -u root -p$MARIADB_ROOT_PASSWORD shutdown
-
-exec mysqld_safe
+# Run the server
+mysqld_safe --datadir=/var/lib/mysql
