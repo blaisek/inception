cf855c7bb65ba654ba93bf29af87cf9554e02984
Blaise <blaze.kalonda@gmail.com>
2023-09-11 22:12:45 +0200

    wp script with cote

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -11,25 +11,27 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 
 # Crée le fichier de configuration wp-config.php
+
 wp core config \
-	--dbname=$MARIADB_NAME \
-	--dbuser=$MARIADB_USER \
-	--dbpass=$MARIADB_PASSWORD \
-	--dbhost=$MARIADB_HOST \
+	--dbname="$MARIADB_NAME" \
+	--dbuser="$MARIADB_USER" \
+	--dbpass="$MARIADB_PASSWORD" \
+	--dbhost="$MARIADB_HOST" \
 	--allow-root
+
 
 wp core install \
   --url="https://$DOMAIN" \
   --title="$WP_TITLE" \
-	--admin_name=$WP_ADMIN_USER \
-	--admin_password=$WP_ADMIN_PASSWORD \
-	--admin_email=$WP_ADMIN_EMAIL \
+	--admin_name="$WP_ADMIN_USER" \
+	--admin_password="$WP_ADMIN_PASSWORD" \
+	--admin_email="$WP_ADMIN_EMAIL" \
 	--allow-root
 
-wp user create $WP_USER \
-	$WP_EMAIL \
+wp user create "$WP_USER" \
+	"$WP_EMAIL" \
 	--role=author \
-	--user_pass=$WP_PASSWORD \
+	--user_pass="$WP_PASSWORD" \
 	--allow-root
 
 wp plugin install redis-cache --activate --allow-root
