4be3b08e7847f61e9e9b5a8cc78d8128978002e7
Blaise <blaze.kalonda@gmail.com>
2023-09-13 22:41:58 +0200

    maria user

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -33,6 +33,8 @@ mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MARIAD
 mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USER}';"
 mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
+#check value of mariadb user
+echo "mariadb user: ${MARIADB_USER}"
 # Gracefully stop MariaDB
 mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} shutdown
 
