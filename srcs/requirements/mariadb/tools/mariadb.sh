5ea851bb1001dbfd992164433598ae52d6f18866
Blaise <blaze.kalonda@gmail.com>
2023-09-16 02:00:10 +0200

    new script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,38 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-# Check if MariaDB is initialized
-if [ ! -f /var/lib/mysql/initialized ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    touch /var/lib/mysql/initialized
-fi
+service mariadb start
 
-mkdir -p /run/mysql
-chown -R mysql:mysql /run/mysqld
-chown -R mysql:mysql /var/lib/mysql
+sleep 10
 
-# Start MariaDB
-mysqld --user=mysql --datadir=/var/lib/mysql &
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_NAME}\`;"
 
-pid=$!
+mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%'\
+		 IDENTIFIED BY '${MARIADB_PASSWORD}';"
 
-# Wait for MariaDB to become available
-until mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} ping >/dev/null 2>&1; do
-    sleep 1
-done
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_NAME}\`.* \
+		TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');
+mariadb -e "CREATE USER IF NOT EXISTS \`root\`@'%'\
+		 IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 
-# create wordpress database and user
-mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_NAME};"
-mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USER}';"
-mysql -u root -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mariadb -e "FLUSH PRIVILEGES;"
 
-# Gracefully stop MariaDB
+# service mariadb stop
+
 mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} shutdown
 
-# Wait for MariaDB to stop
-wait "$pid"
 
-# Start MariaDB again in the foreground
-exec mysqld --user=mysql
+exec mysqld_safe #run in the foreground
