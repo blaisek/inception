66047bb46f4a8f981486f856073a3f7916b52287
Blaise <blaze.kalonda@gmail.com>
2023-09-16 02:41:02 +0200

    maria sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,25 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-service mariadb start
+# -e <=> --execute
 
-sleep 10
-
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_NAME}\`;"
-
-mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%'\
-		 IDENTIFIED BY '${MARIADB_PASSWORD}';"
-
-mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_NAME}\`.* \
-		TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-
-mariadb -e "CREATE USER IF NOT EXISTS \`root\`@'%'\
-		 IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-
-mariadb -e "FLUSH PRIVILEGES;"
-
-# service mariadb stop
-
-mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} shutdown
-
-
-exec mysqld_safe #run in the foreground
+service mysql start;
+mysql --execute "CREATE DATABASE IF NOT EXISTS \`${MARIADB_NAME}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_NAME}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+# restart mariadb
+mysqladmin -u root -p$MARIADB_ROOT_PASSWORD shutdown
+exec mysqld_safe
